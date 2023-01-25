5f9752cb88b8dffbe482defb53866ff4e0ba8085
dmytrokosolapenko <dmitriy.kosolapenko@gmail.com>
2023-01-25 15:28:12 +0200

    configure ktlint plugin

--- app/src/main/java/com/mvvm/sharednotes/MainActivity.kt
+++ app/src/main/java/com/mvvm/sharednotes/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.mvvm.sharednotes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
