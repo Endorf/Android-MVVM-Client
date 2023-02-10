0fc97f6b4e89aa606bf30b39169291b50910338b
Endorf <dmytro.kosolapenko@gmail.com>
2023-02-10 16:10:45 +0200

    fix formatting

--- app/src/main/java/com/mvvm/sharednotes/NotesListFragment.kt
+++ app/src/main/java/com/mvvm/sharednotes/NotesListFragment.kt
@@ -14,13 +14,13 @@ class NotesListFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
 
         _binding = FragmentNotesListBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onDestroyView() {
