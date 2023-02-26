e68fb57e0acff93cf97f3f1fa983710a87310b98
Endorf <dmytro.kosolapenko@gmail.com>
2023-02-26 20:43:30 +0200

    implement user storage

---
+++ core/storage/src/main/java/com/mvvm/sharednotes/storage/realm/LocalUserDataStore.kt
@@ -0,0 +1,6 @@
+package com.mvvm.sharednotes.storage.realm
+
+import com.mvvm.sharednotes.storage.LocalDataStore
+import com.mvvm.sharednotes.storage.realm.entity.User
+
+interface LocalUserDataStore : LocalDataStore<User>
---
+++ core/storage/src/main/java/com/mvvm/sharednotes/storage/realm/RealmDB.kt
@@ -0,0 +1,32 @@
+package com.mvvm.sharednotes.storage.realm
+
+import com.mvvm.sharednotes.storage.BuildConfig
+import com.mvvm.sharednotes.storage.realm.entity.db.AddressEntity
+import com.mvvm.sharednotes.storage.realm.entity.db.PostEntity
+import com.mvvm.sharednotes.storage.realm.entity.db.UserEntity
+import io.realm.kotlin.Realm
+import io.realm.kotlin.RealmConfiguration
+
+interface RealmDB {
+
+    companion object {
+
+        fun beginTransaction(): Realm = realm!!
+
+        fun initializeRealm() = RealmConfiguration.Builder(
+            setOf(
+                UserEntity::class, AddressEntity::class,
+                PostEntity::class
+            )
+        )
+            .name(BuildConfig.DB_NAME)
+            .schemaVersion(BuildConfig.DB_VERSION)
+            .deleteRealmIfMigrationNeeded()
+            .build()
+            .also { config ->
+                realm = Realm.open(config)
+            }
+
+        private var realm: Realm? = null
+    }
+}
