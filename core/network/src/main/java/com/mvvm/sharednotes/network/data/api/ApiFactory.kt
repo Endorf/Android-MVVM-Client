f1944cdc6bb02aed4aa37cc233870a62c568397c
Endorf <dmytro.kosolapenko@gmail.com>
2023-02-26 21:09:49 +0200

    mark as deprecated

--- core/network/src/main/java/com/mvvm/sharednotes/network/data/api/ApiFactory.kt
+++ core/network/src/main/java/com/mvvm/sharednotes/network/data/api/ApiFactory.kt
@@ -6,6 +6,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+@Deprecated("Unused because of Hilt")
 class ApiFactory<T>(private val kClass: Class<T>) {
 
     fun create(baseUrl: String): T {
