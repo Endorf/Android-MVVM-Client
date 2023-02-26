f4dfd273d39420e843b87fec88ecda5d35b25092
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-02-26 20:30:42 +0200

    add network module (#2)

---
+++ core/network/src/main/java/com/mvvm/sharednotes/network/data/storage/RemoteUserDataStoreImpl.kt
@@ -0,0 +1,17 @@
+package com.mvvm.sharednotes.network.data.storage
+
+import com.mvvm.sharednotes.network.BuildConfig
+import com.mvvm.sharednotes.network.data.api.ApiFactory
+import com.mvvm.sharednotes.network.data.api.user.UserApi
+import com.mvvm.sharednotes.network.data.api.user.entity.UserEntity
+
+class RemoteUserDataStoreImpl : RemoteUserDataStore {
+
+    private val api: UserApi = ApiFactory(UserApi::class.java).create(BuildConfig.BASE_URL)
+
+    override suspend fun create(value: String?): Result<UserEntity> =
+        api.runCatching { create(UserEntity(email = value)) }
+
+    override suspend fun read(id: Int): Result<UserEntity> =
+        api.runCatching { get(id) }
+}
