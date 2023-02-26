23a5deb86dbebc9a82082a0e75192dbc407921e0
Endorf <dmytro.kosolapenko@gmail.com>
2023-02-26 20:55:42 +0200

    fix checks

--- core/storage/src/main/java/com/mvvm/sharednotes/storage/exception/UserNotFoundException.kt
+++ core/storage/src/main/java/com/mvvm/sharednotes/storage/exception/UserNotFoundException.kt
@@ -1,6 +1,6 @@
 package com.mvvm.sharednotes.storage.exception
 
-class UserNotFoundException:NullPointerException(MESSAGE) {
+class UserNotFoundException : NullPointerException(MESSAGE) {
 
     companion object {
         private const val MESSAGE = "User not found"
