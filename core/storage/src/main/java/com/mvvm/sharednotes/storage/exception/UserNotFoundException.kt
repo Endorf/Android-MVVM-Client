e403ad3e18e6d716f47a5ac714fdcc14d8465d9a
Endorf <dmytro.kosolapenko@gmail.com>
2023-02-26 20:42:19 +0200

    add exception

---
+++ core/storage/src/main/java/com/mvvm/sharednotes/storage/exception/UserNotFoundException.kt
@@ -0,0 +1,8 @@
+package com.mvvm.sharednotes.storage.exception
+
+class UserNotFoundException:NullPointerException(MESSAGE) {
+
+    companion object {
+        private const val MESSAGE = "User not found"
+    }
+}
