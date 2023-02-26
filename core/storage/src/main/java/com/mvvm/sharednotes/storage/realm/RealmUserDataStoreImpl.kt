2e29531d75a1ea7761e6511d14244d846fcd645c
Endorf <dmytro.kosolapenko@gmail.com>
2023-02-26 21:14:26 +0200

    update realm dataStore

--- core/storage/src/main/java/com/mvvm/sharednotes/storage/realm/RealmUserDataStoreImpl.kt
+++ core/storage/src/main/java/com/mvvm/sharednotes/storage/realm/RealmUserDataStoreImpl.kt
@@ -5,17 +5,21 @@ import com.mvvm.sharednotes.storage.realm.entity.db.UserEntity
 import com.mvvm.sharednotes.storage.realm.entity.mapper.asResult
 import com.mvvm.sharednotes.storage.realm.entity.mapper.toUser
 import com.mvvm.sharednotes.storage.realm.entity.mapper.toUserEntity
+import io.realm.kotlin.Realm
+import javax.inject.Inject
 
-class RealmUserDataStoreImpl : LocalUserDataStore {
+class RealmUserDataStoreImpl @Inject constructor(
+    private val realm: Realm
+) : LocalUserDataStore {
 
     override suspend fun create(value: User) {
-        RealmDB.beginTransaction().write {
+        realm.write {
             copyToRealm(value.toUserEntity())
         }.toUser()
     }
 
-    override suspend fun read(value: User): Result<User?> =
-        RealmDB.beginTransaction().query(
+    override suspend fun read(value: User): Result<User?> = realm
+        .query(
             UserEntity::class, "email == $0", "${value.email}"
         ).first().find()?.toUser().asResult()
 }
