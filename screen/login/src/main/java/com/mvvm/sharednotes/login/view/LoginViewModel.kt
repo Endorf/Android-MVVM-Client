3ed5eda6239f1448b35c7c3d000a4c16258da13f
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-03-21 16:11:39 +0200

    Update LoginViewModel (#17)

--- screen/login/src/main/java/com/mvvm/sharednotes/login/view/LoginViewModel.kt
+++ screen/login/src/main/java/com/mvvm/sharednotes/login/view/LoginViewModel.kt
@@ -1,12 +1,17 @@
 package com.mvvm.sharednotes.login.view
 
 import android.util.Patterns
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.liveData
+import androidx.lifecycle.switchMap
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import com.mvvm.sharednotes.login.data.exception.EmailNotValidException
 import com.mvvm.sharednotes.login.domain.Interactor
+import com.mvvm.sharednotes.login.view.entity.User
 import com.mvvm.sharednotes.login.view.entity.state.ErrorState
 import com.mvvm.sharednotes.login.view.entity.state.LoginUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -23,29 +28,49 @@ class LoginViewModel @Inject constructor(
 
     val keyBoardVisibilityState: MutableLiveData<Boolean> by lazy { MutableLiveData<Boolean>() }
 
-    val inProgressState: MutableLiveData<Boolean> by lazy { MutableLiveData<Boolean>() }
-    val errorState: MutableLiveData<ErrorState> by lazy { MutableLiveData<ErrorState>() }
+    val errorState: LiveData<ErrorState>
 
-    val loginState: MutableLiveData<LoginUiState> by lazy { MutableLiveData<LoginUiState>() }
+    private val _inProgressTest: MutableLiveData<Boolean> by lazy { MutableLiveData<Boolean>() }
+    val inProgressState: LiveData<Boolean>
+
+    val loginState: LiveData<LoginUiState>
+
+    private val sourceLiveData = MutableLiveData<Result<User?>>()
+
+    init {
+        with(MediatorLiveData<Boolean>()) {
+            addSource(sourceLiveData) { value = false }
+            addSource(_inProgressTest) { value = it }
+            inProgressState = this
+        }
+
+        errorState = sourceLiveData.switchMap { result ->
+            liveData {
+                if (result.isFailure) {
+                    result.exceptionOrNull()?.let { emit(ErrorState(it, it.message)) }
+                }
+            }
+        }
+        loginState = sourceLiveData.switchMap { result ->
+            liveData {
+                if (result.isFailure) {
+                    LoginUiState(true, result.getOrNull())
+                } else {
+                    LoginUiState(false)
+                }
+            }
+        }
+    }
 
     fun login() {
         keyBoardVisibilityState.value = false
-        val validationStatus = validateUserInput {
-            errorState.value = ErrorState.create(it)
+        val validationStatus = validateUserInput { error ->
+            error?.let { sourceLiveData.value = Result.failure(it) }
         }
 
         if (validationStatus) viewModelScope.launch {
-            inProgressState.value = true
-            val result = interactor.login(userInput)
-            when {
-                result.isSuccess -> loginState.value = LoginUiState(true, result.getOrNull())
-                result.isFailure -> {
-                    loginState.value = LoginUiState(false)
-                    errorState.value =
-                        result.exceptionOrNull()?.let { ErrorState(it, it.message) }
-                }
-            }
-            inProgressState.value = false
+            _inProgressTest.value = true
+            sourceLiveData.value = interactor.login(userInput)
         }
     }
 
