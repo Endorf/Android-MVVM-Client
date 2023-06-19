f14dedb2020b53f887172ee0d39fa1d501553bc6
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-06-19 21:39:22 +0300

    update splash

--- app/src/main/java/com/mvvm/sharednotes/RoutingActivityViewModel.kt
+++ app/src/main/java/com/mvvm/sharednotes/RoutingActivityViewModel.kt
@@ -8,8 +8,12 @@ class RoutingActivityViewModel : ViewModel() {
     val isUserSignedIn: MutableLiveData<Boolean> by lazy {
         MutableLiveData<Boolean>()
     }
+    val keepScreenOnCondition: MutableLiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>()
+    }
 
     fun retrieveUser() {
+        keepScreenOnCondition.value = false
         isUserSignedIn.value = false
     }
 }
